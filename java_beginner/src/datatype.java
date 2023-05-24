5a138527467b7c482d2c38af6996fee896007104
yatharthnanda <yatharthnanda25@gmail.com>
2023-05-24 19:02:44 +0530

    adding beginner folder

---
+++ java_beginner/src/datatype.java
@@ -0,0 +1,10 @@
+public class datatype {
+    public static void main (String[] args) {
+        int a=2;
+        double b=10.35;
+        char c = 'A';
+        System.out.println(c);
+        System.out.println(b/a);
+
+    }
+}
